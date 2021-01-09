225a516c8f7a97df742bd413ab699fada424af84
Mohammad <mohammadidehgamwala5@gmail.com>
2021-01-09 15:46:27 +0530

    ignored config

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,7 @@ import firebase from '@react-native-firebase/app';
 import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyA6d0IbXEUPbEvRXg5nw_ZPKcDYPYSD2CQ',
-  authDomain: 'mywatchlist-ffe48.firebaseapp.com',
-  projectId: 'mywatchlist-ffe48',
-  storageBucket: 'mywatchlist-ffe48.appspot.com',
-  messagingSenderId: '785790723019',
-  databaseURL: '',
-  appId: '1:785790723019:web:e19989cf066ada9caacc4e',
-};
+import {firebaseConfig} from './firebase.config';
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
