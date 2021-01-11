6c8782f398d67a65de930969c6edbb65b8c10aee
Mohammad <mohammadidehgamwala5@gmail.com>
2021-01-11 17:13:19 +0530

    resolved

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,10 @@ import firebase from '@react-native-firebase/app';
 import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
 
-import { firebaseConfig } from './firebase.config';
+import firebaseConfig from './firebase.config';
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
-export { firebase as default, auth };
+export {firebase as default, auth};
